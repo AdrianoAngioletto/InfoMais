433633176f0d28ecce10a7ae5112ebba14a3b8be
AdrianoAngioletto <angiolettobrasil@icloud.com>
2024-03-06 22:52:38 -0300

    atualizando novos metodos

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from selenium.webdriver import Chrome, ChromeOptions
 from selenium.webdriver.common.by import By
 from time import sleep
+import pyautogui
+import json
+
 
 """
 author =  AdrianoAngioletto
@@ -35,7 +38,7 @@ class Desafio:
         print(bemvindo)
         
     
-    def EncontraElementos(self, valor: str, Element: object =By.XPATH,):
+    def EncontraElementos(self, valor: str, Element: object =By.XPATH,) -> object:
         
             # função para facilitar a vida, pegar o elemento sem necessidade de ficar escrevendo find .. etc
                                                      
@@ -46,16 +49,26 @@ class Desafio:
         
      
 
-    def IniciaProdutos(self):
+    def IniciaProdutos(self)-> None:
             
         # função responsavel por iniciar o site, pegar os elementos relacionado a Produtos e converter o 
         # objeto no tipo de dado necessario.
         
-        print(' Estamos, Carregando o site por favor aguarde')
-        
         self.drive.get('https://infosimples.com/vagas/desafio/commercia/product.html')
         
         sleep(5) # tempo para navegador carregar pagina
+        
+                
+        javascript = 'window.alert("BEM VINDO AO ROBÔ, INFO-MAIS-DESAFIO, Aguarde iremos pegar todos dados")'
+        
+        self.drive.execute_script(javascript)
+
+        
+        sleep(4) # time obrigatorio para o aviso em javascript 
+        
+        pyautogui.hotkey('enter') # só para o javascript não parar a exec, se caso usuario nao apertar em nada
+        
+
         
         produto1 = self.EncontraElementos('/html/body/div/section/div/div[2]/div[1]/div[1]/div[3]/div[1]') # PRODUTO 1 EM OBJETO
         
@@ -65,9 +78,9 @@ class Desafio:
         
         self.produto1_marca = produto1_marca.text # PRODUTO 1 MARCA EM STRING
         
-        produto1_categoria = self.EncontraElementos('/html/body/div/section/div/nav/a[4]') # PRODUTO 1 CATEGORIA OBJETO 
+        produto1_categoria = self.EncontraElementos('/html/body/div/section/div/nav') # PRODUTO 1 CATEGORIA OBJETO 
         
-        self.produto1_categoria = produto1_categoria.text #PRODUTO 1 CATEGORIA EM STRING
+        self.produto1_categoria = produto1_categoria.text.replace('>' , ' ') #PRODUTO 1 CATEGORIA EM STRING
         
         descricao_geral_dos_produtos = self.EncontraElementos('/html/body/div/section/div/div[3]') # OBJETO DIV, ONDE FICA DESCRICAO DOS PRODUTOS
         
@@ -75,28 +88,162 @@ class Desafio:
         
         self.descricao_edit = self.descricao.replace("Description", "") # Aqui só removi a palavra Description pq não é necessaria.
         
-       
+        valor_produto_1_atual = self.EncontraElementos('/html/body/div/section/div/div[2]/div[1]/div[1]/div[3]/div[2]') # PEGANDO OBJETO
+        
+        valor_produto_1_antigo = self.EncontraElementos('/html/body/div/section/div/div[2]/div[1]/div[1]/div[3]/div[3]') # OBJETO
 
-        def IniciaPropriedadesProduto(self):
-                
-                
-                
-                # funcão responsavel por pegar Propriedades como manda o PDF.
-                
-                
-                
-                
+        texto_valor_produto_1_atual = valor_produto_1_atual.text # CONVERTENDO EM TEXTO PQ É UM OBJETO
         
-                
-                ...
-                
-                
-                
-                
+        texto_valor_produto_1_antigo = valor_produto_1_antigo.text # CONVERTENDO EM TEXTO PQ É UM OBJETO
         
+        # CONVERTENDO PARA FLOAT E TIRANDO O $
+        self.valor_produto_1_atual_float = float(texto_valor_produto_1_atual.replace("R$ ", "").replace(",", "."))
+        self.valor_produto_1_antigo_float = float(texto_valor_produto_1_antigo.replace("R$ ", "").replace(",", "."))
+
+        print(f'valor novo {self.valor_produto_1_atual_float}, valor veio {self.valor_produto_1_antigo_float}')
+
+        
+        
+
+
+        print(f'valor novo {self.valor_produto_1_atual_float},', f' valor veio {self.valor_produto_1_antigo_float}')
+       
+    def IniciaPropriedadesProduto(self)-> None:
+                
+         # funcão responsavel por pegar Propriedades como manda o PDF.       
+        tabela_produtos = self.EncontraElementos('/html/body/div/section/div/div[4]/table') # TABELA TODA OBJETO
+                
+        self.texto_tabela = tabela_produtos.text # TABELA EM TEXTO
+        
+        tabela_produtos_adicionais = self.EncontraElementos('//*[@id="propadd"]/table') # TABELA PRODUTOS ADICIONAIS OBJETO
+        
+        self.texto_tabela_adicionais = tabela_produtos_adicionais.text  # TABELA ADICIONAL TEXTO
+        
+        # ESSE IF É PARA TRATAR OS -  E PALAVRA NONE, MÁS LEGIVEL.
+        if '-' in self.texto_tabela_adicionais or self.texto_tabela_adicionais == 'None':
+                           
+                self.texto_tabela_adicionais = self.texto_tabela_adicionais.replace('-', '0')
+                self.texto_tabela_adicionais = self.texto_tabela_adicionais.replace('None', '0')
+                
+        if 'Storage temperature 0 0 25ºC' in self.texto_tabela_adicionais:
+                # AQUI PARA ADICIONAR DE VOLTA O - QUE FOI RETIRADO NO PRIMEIRO IF
+                self.texto_tabela_adicionais = self.texto_tabela_adicionais.replace('Storage temperature 0 0 25ºC', 'Storage temperature 0 - 25ºC')
+
+    def calcular_media_avaliacoes(*avaliacoes)-> float:
+        
+ 
+        # AQUI TOU ASSUMINDO QUE 1 ESTRELA, EQUIVALE HÁ 1. DAI POR DIANTE
+        mapeamento_estrelas = {'★': 1, '★★': 2, '★★★': 3, '★★★★': 4, '★★★★★': 5}
+        
+        # Converte as avaliações para valores numéricos e calcula a média
+        valores_avaliacoes = [mapeamento_estrelas.get(avaliacao, 0) for avaliacao in avaliacoes]
+        media_avaliacoes = sum(valores_avaliacoes) / len(valores_avaliacoes)
+        
+        # ARENDONDA DUAS CASAS DECIMAIS.
+        media_avaliacoes = round(media_avaliacoes, 2)
+        
+        
+        return media_avaliacoes
+
+
+    def ProdutosReview(self)-> None:
+            
+         # FUNCAO RESPONSAVEL, POR PEGAR AS REVIEWS.
+         
+            # ----- PRIMEIRA VIEW -------------- #
+
+            self.primeira_review = self.EncontraElementos('//*[@id="comments"]/div[1]')
+            
+            primeira_review =  self.primeira_review.text
+            
+            linhas = primeira_review.split('\n')
+
+            review_dict = {
+                'NOME': linhas[0].strip(),
+                'DATA': linhas[1].strip(),
+                'SCORE': linhas[2].strip(),
+                'AVALIAÇÃO': linhas[3].strip()
+                }
+            
+    
+            # ----- FIM  DA PRIMEIRA  REVIEW -------------- #
+            
+
+            # ----- Segunda VIEW -------------- #
+            
+            self.segunda_review = self.EncontraElementos('//*[@id="comments"]/div[2]')
+            
+            segunda_review =  self.segunda_review.text
+            
+            linhas = segunda_review.split('\n')
+
+            review2_dict = {
+                'NOME': linhas[0].strip(),
+                'DATA': linhas[1].strip(),
+                'SCORE': linhas[2].strip(),
+                'AVALIAÇÃO': linhas[3].strip()
+                }
+            
+          # ----- FIM DA SEGUNDA REVIEW -------------- #
+          
+          
+            # ----- TERCEIRA VIEW -------------- #
+            
+            self.terceira_review = self.EncontraElementos('//*[@id="comments"]/div[3]')
+            
+            terceira_review =  self.terceira_review.text
+            
+            linhas = terceira_review.split('\n')
+
+            review3_dict = {
+                'NOME': linhas[0].strip(),
+                'DATA': linhas[1].strip(),
+                'SCORE': linhas[2].strip(),
+                'AVALIAÇÃO': linhas[3].strip()
+                }
+            
+            
+            # ----- FIM DA TERCEIRA REVIEW -------------- #
+            
+            
+            # PARTE DA MEDIA BASEADO NAS ESTRELAS
+            self.avaliacao1 = '★★★★☆'
+            self.avaliacao2 = '★☆☆☆☆'
+            self.avaliacao3 = '★★★★★'
+        
+            media_avaliacoes = self.calcular_media_avaliacoes(self.avaliacao1, self.avaliacao2, self.avaliacao3)
+            
+            url_pagina = 'https://infosimples.com/vagas/desafio/commercia/product.html'
+    
+    def ConverteJson(self):
+            
+        # funcao responsavel por salvar os arquivos json.
+            
+        json_produtos = {
+            'produto1Texto': self.produto1Texto,
+            'produto1_marca': self.produto1_marca,
+            'produto1_categoria': self.produto1_categoria,
+            'descricao_edit': self.descricao_edit,
+            'valor_produto_1_atual_float': self.valor_produto_1_atual_float,
+            'valor_produto_1_antigo_float': self.valor_produto_1_antigo_float,
+            'texto_tabela': self.texto_tabela,
+            'texto_tabela_adicionais': self.texto_tabela_adicionais,
+            'media_avaliacoes': self.calcular_media_avaliacoes(self.avaliacao1, self.avaliacao2, self.avaliacao3),
+            'url_pagina': 'https://infosimples.com/vagas/desafio/commercia/product.html'
+        }
+
+        
+        json_data = json.dumps(json_produtos, indent=2)
+
+
+        print(json_data)
+            
+ 
+        
+                        
     
 desafio = Desafio()
-
 desafio.IniciaProdutos()
-        
-        
+desafio.IniciaPropriedadesProduto()
+desafio.ProdutosReview()
+desafio.ConverteJson()
